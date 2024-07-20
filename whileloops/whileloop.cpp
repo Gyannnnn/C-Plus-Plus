956428254ec7840b786834fa4ff5ff63d68bd9cc
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-20 15:45:16 +0530

    Do While Loop

---
+++ whileloops/whileloop.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i = 1;
+    while(i<=10){
+        cout<<"Gyanranjan"<<i<<endl;
+        i++;
+
+    }
+
+    //
+    // do while loop
+
+    int j = 2;
+    do{
+        cout<<"Gyanranjan Patra"<<j;
+        j++;
+    }while(j<=1);
+    // do while loop is used when we want to execute atleast once
+    return 0;
+}
