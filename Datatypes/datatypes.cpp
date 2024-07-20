cba5f3a642ffb0e14a20da5b6f93f44fe47469e9
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-20 11:46:58 +0530

    Initial Commit

---
+++ Datatypes/datatypes.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // int
+    //  int x = 10;
+    //  //long
+    //  long x =10;
+    //  //long long
+    //  long long y = 1828860893678578;
+
+    // float double
+
+    // float b = 90.0; // 32 bit range is comparatively lower than the double
+    // double c = 90.0000000000; //64 bit range is wider
+
+    // string
+
+    // string s;
+    // getline(cin,s); // pick the entire line otherwise it will pic before the " "
+    // cout<<s;
+
+    // character
+    char ch = 'g'; // single quote for characters double quotes for string
+    cout << ch;
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,22 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int x, y;
+//     cin >> x >> y;
+//     cout << "The Value Of x:" << x << "\t" << "Value Of Y:" << y;
+//     return 0;
+// }
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter The Value Of X"<<"\t";
+    cin>>x;
+    cout<<"Enter The Value of Y"<<"\t";
+    cin>>y;
+    cout<<"The Required Sum Is "<<x+y;    
+}
