00b327b7ed11997a54622c485039c57025c8cad9
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-20 16:04:00 +0530

    Parametric function

---
+++ Function/function.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+    //void function -> does not return anything;
+
+void printName(string name){
+    cout<<"Hey "<<name;
+}
+
+int main(){
+    string name;
+    cin>>name;
+    printName(name);
+
+    string name2;
+    cin>>name2;
+    printName(name2);
+
+    return 0;
+}
---
+++ Function/parameterfunction.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void twoSum(int a,int b){
+    cout<<"The Required Sum Is "<<a+b;
+}
+
+
+int main(){
+    int a,b;
+    cout<<"Enter The Value Of The Two Numbers";
+    cin>>a>>b;
+    twoSum(a,b);
+
+    return 0;
+}
