644736f947c9e6725327cacddc86f0a96dd22438
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-20 16:17:57 +0530

    Functions

--- Function/parameterfunction.cpp
+++ Function/parameterfunction.cpp
@@ -5,13 +5,20 @@ int  twoSum(int a,int b){
     return a+b;
 }
 
+int maxx(int a,int b,int c,int d ,int e,int f){
+  if
+
+}
 
 int main(){
-    int a,b;
+    int a,b,c,d,e,f;
     cout<<"Enter The Value Of The Two Numbers";
     cin>>a>>b;
     int res = twoSum(a,b);
     cout<<"Required Sum Is "<<res;
+    cout<<"Enter The Numbers ";
+    cin>>a>>b>>c>>d>>e>>f;
+    int maximum = maxx(a,b,c,d,e,f);
 
     return 0;
 }
