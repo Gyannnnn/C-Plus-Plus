99e214831304afaf7d7655e4b1545380dbd8edc8
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-24 19:31:17 +0530

    pairs

--- stl/pairs.stl.cpp
+++ stl/pairs.stl.cpp
@@ -1,8 +1,15 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main(){
-    pair<int,int> p={1,3};
-    cin>>p.first>>p.second;
-    cout<<p.first<<p.second;
+
+int main() {
+    // Initialize a pair of integers
+    pair<int, int> p = {1, 3};
+    
+    // Read input values into the pair
+    cin >> p.first >> p.second;
+    
+    // Output the values of the pair
+    cout << p.first << " " << p.second;
+    
     return 0;
 }
