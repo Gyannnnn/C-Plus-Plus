13266d60c7e90961b3c76b2549dcef50dc20fbde
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-24 19:28:27 +0530

    pairs

---
+++ stl/pairs.stl.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    pair<int,int> p={1,3};
+    cin>>p.first>>p.second;
+    cout<<p.first<<p.second;
+    return 0;
+}
---
+++ stl/stl.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(){
+    cout<<"Gyanranjan ";
+}
+
+int main(){
+    print();
+    return 0;
+}
